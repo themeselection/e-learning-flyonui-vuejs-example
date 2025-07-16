70390b889620272f1d56c942431adaf055258103
Deep Kumbhare <kumbharedeep85@gmail.com>
2025-07-16 15:42:54 +0530

    fix: reorder import statements in vite.config.js for consistency

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  // base: '/examples/mcp/e-learning/',
 });
